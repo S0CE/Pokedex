3d1a8e37795226433dae16d41fc853871b98e8cf
Laurent ZHANG <Soce@2a02-8428-000b-ad01-3a9c-e4db-4834-d768.rev.sfr.net>
2023-06-11 13:59:24 +0200

    feat(backend): get signal and check if request is correct

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body, HttpException } from '@nestjs/common';
 import { AppService, PokemonService } from './app.service';
 
 @Controller()
@@ -6,8 +6,14 @@ export class AppController {
   constructor(private readonly appService: AppService,
     private readonly pokemonService: PokemonService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    @Post('/api')
+  async getPokemon(@Body() id: any) {
+    const onlyNumbers = this.appService.is_number(id.number);
+
+    if (onlyNumbers === true)
+      return this.pokemonService.getPokemonInfo(id.number);
+    else {
+      throw new HttpException('Bad format', 404)
+    }
   }
 }

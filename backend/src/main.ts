b2e4dc7990a22d6b9a76c7e5e54567727d9548af
Laurent ZHANG <Soce@2a02-8428-000b-ad01-3a9c-e4db-4834-d768.rev.sfr.net>
2023-06-11 13:12:19 +0200

    feat: connect front to back

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT: number = 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`Le serveur est lancé sur le port: ${PORT}`);
+  });
 }
 bootstrap();

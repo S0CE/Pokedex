642f1df9f11f7a6d6405c6fa2153cf5db90d5b6a
Laurent ZHANG <Soce@2a02-8428-000b-ad01-3a9c-e4db-4834-d768.rev.sfr.net>
2023-06-11 13:55:28 +0200

    feat(backend): create fonction used getPokemonbyId from PokeAPI && function check if number or not

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import {} from 'pokenode-ts';
+import { PokemonClient } from 'pokenode-ts';
+
+const PokemonAPI = new PokemonClient;
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getMessage(str: string): string {
+    return str;
+  }
+
+  is_number(str: string): boolean {
+    for (let i = 0; i < str.length; i++) {
+      if (isNaN(parseInt(str[i])))
+        return false
+    }
+    return true;
   }
 }
 
 @Injectable()
 export class PokemonService {
-  
+  async getPokemonInfo(id: number) {
+    const result = await PokemonAPI.getPokemonById(id)
+    return result;
+  }
 }
